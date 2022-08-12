ac9cae34f7d09eb5e17d7e6ba8803e3a0cd06b5f
GhosteDeath <101995937+nrikas@users.noreply.github.com>
2022-08-12 13:40:46 +0100

    Create store.php

---
+++ public_html/store.php
@@ -0,0 +1,23 @@
+<html>
+<br>
+<br>
+<a>Bank Transfer Any Country i do $100  for $1000 or £400  for £4000</a>
+<p>
+<br>
+<a>CashApp i do $100  for $1000 or £500  for £5000<a>
+ <br>
+  <a>FraudFox Box  $89.99</a>
+  <a>Fullz page $39.99<a>
+    <a>BLogs page $59.99<a>
+      <a>Custom Page $69.99<a>
+<br>
+<p>Amount = Item Name this is how i know what item your buying</p>
+<pre>Please input amount and hit donate once completed message me on t.me/ghostedsecdev</pre>
+<iframe
+  src="https://nowpayments.io/embeds/donation-widget
+  ?api_key=5C42R42-FCVMJ0R-HXPPZ7F-Z4031V4"
+  width="354" height="680" frameborder="0"
+  scrolling="no" style="overflow-y: hidden;">
+    Can't load widget
+</iframe>
+        </html>
